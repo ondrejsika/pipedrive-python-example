ceaef72a505ebfd805bf7c989ac055f2f1f01ef1
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-16 18:26:49 +0200

    Add Pipedrive CLI client with only create deal functionality

---
+++ pd.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+from pipedrive import Pipedrive
+
+
+ENV_PIPEDRIVE_TOKEN = os.environ.get("PIPEDRIVE_TOKEN")
+
+
+root_parser = argparse.ArgumentParser()
+root_parser.add_argument(
+    "-t", "--token", default=ENV_PIPEDRIVE_TOKEN, required=not bool(ENV_PIPEDRIVE_TOKEN)
+)
+root_subparsers = root_parser.add_subparsers(required=True, dest="command")
+
+deals_parser = root_subparsers.add_parser("deals")
+deals_subparsers = deals_parser.add_subparsers(required=True, dest="command2")
+
+deals_create_parser = deals_subparsers.add_parser("create")
+deals_create_parser.add_argument("--title", required=True)
+deals_create_parser.add_argument("--value", required=True, type=int)
+deals_create_parser.add_argument("--currency", required=True)
+
+args = root_parser.parse_args()
+
+
+pipedrive = Pipedrive(args.token)
+
+
+def deals_create():
+    pipedrive.deals(
+        {
+            "title": args.title,
+            "value": args.value,
+            "currency": args.currency,
+            "status": "open",
+        },
+        method="POST",
+    )
+
+
+{("deals", "create"): deals_create,}[(args.command, args.command2)]()
